9c8441ded331bcfc3a5afbd06425778242d44594
Garali Mohamed Karim <garalimedkarim2@gmail.com>
2018-03-03 00:36:16 +0100

    9.Configuring our Maps

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ModalController } from 'ionic-angular';
 import { SetLocationPage } from '../set-location/set-location';
+import { Location } from '../../models/location';
 
 @Component({
   selector: 'page-add-place',
   templateUrl: 'add-place.html',
 })
 export class AddPlacePage {
+  location:Location = {lat:36.731470384526965, lan:10.210593179614534};
 
   constructor(private modalCtrl:ModalController,
               ) {
@@ -18,7 +20,7 @@ export class AddPlacePage {
   }
 
   onOpenMap(){
-    let modal = this.modalCtrl.create(SetLocationPage);
+    let modal = this.modalCtrl.create(SetLocationPage,{ location:this.location } );
     modal.present();
   }
 
