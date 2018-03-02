4a88b08e2bcb4d95f5d01c162f9284e366e079e4
Garali Mohamed Karim <garalimedkarim2@gmail.com>
2018-03-02 15:11:52 +0100

    6.Filling the 'New Place' Page with Life (incl. Template-Driven Form)

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
 
-/**
- * Generated class for the AddPlacePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-add-place',
   templateUrl: 'add-place.html',
@@ -18,8 +11,9 @@ export class AddPlacePage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddPlacePage');
+  onSubmit(form:NgForm){
+    console.log(form.value);
   }
+
 
 }
