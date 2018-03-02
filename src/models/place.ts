b799203d51aaf6f5da58c282461079bfea4b527b
Garali Mohamed Karim <garalimedkarim2@gmail.com>
2018-03-02 09:21:49 +0100

    2+3.Generating required Pages + Models

---
+++ src/models/place.ts
@@ -0,0 +1,8 @@
+import { Location } from "./location";
+
+export class location{
+    constructor(public title:string,
+                public description:string,
+                public location:Location,
+                public imagePath:string){}
+}
