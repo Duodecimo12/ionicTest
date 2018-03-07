df137d20a2290834d366d1274107a96571989de8
Garali Mohamed Karim <garalimedkarim2@gmail.com>
2018-03-07 14:52:19 +0100

    test 2

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -91,10 +91,10 @@ export class AddPlacePage {
     this.camera.getPicture(options).then((imageData) => {
      // imageData is either a base64 encoded string or a file URI
      // If it's base64:
-     //let base64Image = 'data:image/jpeg;base64,' + imageData;
-     this.imageUrl = imageData;
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+     this.imageUrl = base64Image;
     }, (err) => {
-     // Handle error
+     alert(err);
     });
   }
 
